d5bc1b0578253735f10e1da097b24349c9e3b6f6
torstendngh <torstendngh@gmail.com>
2025-05-12 12:01:33 +0200

    add postinstall script

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['better-sqlite3'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['better-sqlite3'],
+    },
+    commonjsOptions: { include: [] },
+  },
   plugins: [
     tailwindcss(),
     react(),
